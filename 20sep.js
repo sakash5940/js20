27db8ef784c1be1def9e094803f720f0ad6aace4
sakash5940 <akashpatil5940@gmail.com>
2024-09-22 22:53:13 +0530

    Add files via upload

---
+++ 20sep.js
@@ -0,0 +1,12 @@
+var obj={
+    name:"akash",
+    age:22,
+    job:'business'
+}
+obj.name="ganesh",
+obj.age=25,
+obj.job="company",
+obj.location="hyderabad",
+obj.earn=500000,
+
+console.log(obj)
